e167c172cf0394827767462f3f105ff80ab6ed54
Cedric Guillemet <1312968+CedricGuillemet@users.noreply.github.com>
2026-06-25 16:19:43 +0200

    Dawn-V8 experiment

--- native/RunExperiments.ps1
+++ native/RunExperiments.ps1
@@ -1,17 +1,20 @@
 $exeFiles = @(
-    ".\dawn\testapp.exe"
+    @{
+        Path = ".\dawn\app.exe"
+        Args = @("native-lite-benchmark.lite.js", "--no-vsync", "--frames", "1000")
+    }
 )
 
 foreach ($exe in $exeFiles) {
-    $exePath = Resolve-Path $exe
+    $exePath = Resolve-Path $exe.Path
     $exeDir  = Split-Path $exePath -Parent
     $exeName = Split-Path $exePath -Leaf
 
-    Write-Host "Running $exeName in $exeDir"
+    Write-Host "Running $exeName $($exe.Args -join ' ') in $exeDir"
 
     Push-Location $exeDir
     try {
-        & ".\$exeName"
+        & ".\$exeName" @($exe.Args)
     }
     finally {
         Pop-Location
